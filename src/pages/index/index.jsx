dade035f776b4db0cd23463a65eb0a1f9f8d0b14
ruyi.g <ruyi.g@sunyard.com>
2020-06-28 22:53:56 +0800

    初始化脚手架 taro

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,48 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
+import './index.less'
+
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add() {
+    dispatch(add())
+  },
+  dec() {
+    dispatch(minus())
+  },
+  asyncAdd() {
+    dispatch(asyncAdd())
+  }
+}))
+class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  render() {
+    return (
+      <View className='index'>
+        <Text className='title'>开始编写音乐播放器</Text>
+      </View>
+    )
+  }
+}
+
+export default Index
